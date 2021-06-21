2cb035c3b0c1f786f20dc53b61658dca04c630d5
Sabrina Ida Martini <i.sabrina.martini@gmail.com>
2021-06-21 00:37:46 -0300

    mi fourth commit - js update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,27 @@
 function navSlide() {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-
-  burger.addEventListener("click", () => {
-    //Toggle Nav
-    nav.classList.toggle("nav-active");
-    
-    //Animate Links
-    navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-            link.style.animation = ""
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-        }
-    });
-    //Burger Animation
-    burger.classList.toggle("toggle");
-});
-
-}
-
-
-navSlide();
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    const body = document.querySelector("body");
+  
+    burger.addEventListener("click", () => {
+      //Toggle Nav
+      nav.classList.toggle("nav-active");
+      body.classList.toggle("blocked");
+      
+      //Animate Links
+      navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+              link.style.animation = ""
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+          }
+      });
+      //Burger Animation
+      burger.classList.toggle("toggle");
+  });
+  
+  }
+  
+  
+  navSlide();
